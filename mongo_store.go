d893b61cced272168f6ac70aa649aff61d819560
wangzhezhe <w_hessen@126.com>
2015-06-30 17:24:02 +0800

    update

--- mongo_store.go
+++ mongo_store.go
@@ -10,6 +10,14 @@ import (
 	"strings"
 )
 
+var (
+	username string
+	password string
+	host     string
+	port     string
+	instance string
+)
+
 type Command struct {
 	Type  string `bson:"type"`
 	Intro string `bson:"intro"`
@@ -20,9 +28,42 @@ type Dockerfile struct {
 	Type     string
 }
 
+func Config() {
+
+	username = os.Getenv("MONGODB_USERNAME")
+	password = os.Getenv("MONGODB_PASSWORD")
+	host = os.Getenv("MONGODB_PORT_27017_TCP_ADDR")
+
+	if len(host) == 0 {
+		host = "localhost"
+	}
+
+	port = os.Getenv("MONGODB_PORT_27017_TCP_PORT")
+	if len(port) == 0 {
+		port = "27017"
+	}
+
+	instance = os.Getenv("MONGODB_INSTANCE_NAME")
+}
+
 func main() {
-	//endpoint:=os.Getenv("")
-	session, err := mgo.Dial("10.10.72.139:27017")
+
+	conn := ""
+	if len(username) > 0 {
+		conn += username
+
+		if len(password) > 0 {
+			conn += ":" + password
+		}
+
+		conn += "@"
+	}
+
+	conn += fmt.Sprintf("%s:%s/%s", host, port, instance)
+
+	session, err := mgo.Dial(conn)
+
+	//session, err := mgo.Dial("10.10.72.139:27017")
 	//session, err := mgo.Dial("10.10.105.204:27017")
 
 	if err != nil {
